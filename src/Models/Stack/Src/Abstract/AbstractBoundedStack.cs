15a6fa8fc22cde9f0b9d056b440cf0766c73f7e7
leonidk <leonidk@brokeree.com>
2020-12-05 21:41:28 +0300

    initialize project

---
+++ src/Models/Stack/Src/Abstract/AbstractBoundedStack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack.Src.Abstract
+{
+    abstract class AbstractBoundedStack <T>
+    {
+        public abstract void Push(T item);
+
+        public abstract void Pop();
+
+        public abstract T Peek();
+
+        public abstract int Size();
+
+        public abstract void Clear();
+
+        public abstract int GetPeekStatus();
+
+        public abstract int GetPopStatus();
+    }
+}
---
+++ src/Models/Stack/Src/BoundedStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Stack.Src.Abstract;
+
+namespace Stack.Src
+{
+    class BoundedStack <T> : AbstractBoundedStack<T>
+    {
+        // data
+        private List<T> _boundedStack;
+        private int _peek_status;
+        private int _pop_status;
+
+        // stack operations 
+
+        public override void Push(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Size()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetPopStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetPeekStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
