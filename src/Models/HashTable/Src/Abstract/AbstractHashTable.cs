e6e35e9b2fb201c4493d107157a328f439233ae1
leonidk <leonidk@brokeree.com>
2020-12-21 17:13:45 +0300

    Implement hastable

--- src/Models/HashTable/Src/Abstract/AbstractHashTable.cs
+++ src/Models/HashTable/Src/Abstract/AbstractHashTable.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HashTable.Enums;
 
-namespace HashTable.Src.Abstract
+namespace HashTable.Abstract
 {
     public abstract class AbstractHashTable <T>
     {
         // initialization
         // Condition after: hashtable created
-        public abstract AbstractHashTable<T> Initialize();
+        public abstract AbstractHashTable<T> Initialize(int size);
+
+        // calculate free slot's index
+        // condition: hashtable initialized 
+        public abstract void HashFunc(T item);
+
+
+        // checks if item are in slots
+        // condition: hashtable is initialized 
+        // condition: 
+        public abstract OperationCode Find(T item);
+
+        // condition: slot is calculated
+        // condition: can place item
+        public abstract void Put(T item);
+
+        // condition: hashtable is initialized
+        public abstract int Size();
+
+        // condition: hashtable is initialized
+        public abstract void SetToNextFreeSlot();
+
+        public abstract OperationCode CanPlaceItem();
+
+        public abstract OperationCode LastInsertStatus();
+
+        public abstract OperationCode LastHashStatus();
     }
 }
--- src/Models/HashTable/Src/HashTableModel.cs
+++ src/Models/HashTable/Src/HashTableModel.cs
@@ -1,12 +1,146 @@
-﻿using HashTable.Src.Abstract;
+﻿using System;
+using System.Linq;
+using HashTable.Abstract;
+using HashTable.Enums;
 
-namespace HashTable.Src
+namespace HashTable
 {
     public class HashTableModel<T> : AbstractHashTable<T>
     {
-        public override AbstractHashTable<T> Initialize()
+        private T[] _values;
+
+        private int? _slot;
+
+        private int _count;
+
+        // statuses 
+
+        private OperationCode _lastHashStatus;
+
+        private OperationCode _canPlaceItemToSlot;
+
+        private OperationCode _lastPutStatus;
+
+
+        public override AbstractHashTable<T> Initialize(int size)
         {
+            _values ??= new T[size];
             return this;
+        }
+
+        public override void HashFunc(T item)
+        {
+            if (_values == null)
+            {
+                _lastHashStatus = OperationCode.NotInitialized;
+                return;
+            }
+            // calculate slot 
+            var itemStr = item.ToString();
+            int hash = 0;
+            for (int i = 0; i < itemStr.Length; i++)
+            {
+                hash += itemStr[i];
+                hash += (hash << 10);
+                hash ^= (hash << 6);
+            }
+
+            _slot = Math.Abs(hash % (_values.Length + 1));
+            _lastHashStatus = OperationCode.Ok;
+            _canPlaceItemToSlot = OperationCode.CanBePlaced;
+
+            if (_values[(int) _slot] == null)
+            {
+                return;
+            }
+
+            // check if free
+            if (!_values[(int) _slot].Equals(default(T)))
+            {
+                _canPlaceItemToSlot = OperationCode.CanNotBePlaced;
+            }
+        }
+
+        public override OperationCode Find(T item)
+        {
+            if (_values == null)
+            {
+                return OperationCode.NotInitialized;
+            }
+
+            if (_values.Contains(item))
+            {
+                return OperationCode.Exists;
+            }
+
+            return OperationCode.NotFound;
+        }
+
+        public override void Put(T item)
+        {
+            var code = Find(item);
+            if (code != OperationCode.NotFound)
+            {
+                _lastPutStatus = OperationCode.Error;
+                return;
+            }
+
+            if (_slot == null)
+            {
+                _lastPutStatus = OperationCode.SlotNotCalculate;
+                return;
+            }
+
+            if (CanPlaceItem() != OperationCode.CanBePlaced)
+            {
+                _lastPutStatus = OperationCode.CanNotBePlaced;
+                return;
+            }
+
+            _values[(int) _slot] = item;
+            _count++;
+
+        }
+
+        public override OperationCode CanPlaceItem()
+        {
+            return _canPlaceItemToSlot;
+        }
+
+        public override OperationCode LastInsertStatus()
+        {
+            return _lastPutStatus;
+        }
+
+        public override OperationCode LastHashStatus()
+        {
+            return _lastHashStatus;
+        }
+
+        public override int Size()
+        {
+            return _count;
+        }
+
+        public override void SetToNextFreeSlot()
+        {
+            if (_slot == null)
+            {
+                return;
+            }
+            
+            if (CanPlaceItem() != OperationCode.CanBePlaced)
+            {
+                var current = _slot;
+                for (; current < _values.Length; current++)
+                {
+                    if (_values[(int)current] == null)
+                    {
+                        _slot = current;
+                        return;
+                    }
+                }
+            }
         }
     }
 }
